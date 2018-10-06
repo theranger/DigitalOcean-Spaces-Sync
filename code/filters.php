7562970303ce9a16f9f57c81c3d1ffb5c89da2ef
keeross <cc@dolsen.co>
2018-10-06 17:03:48 +0300

    Removed old code.

--- code/filters.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-add_filter('wp_generate_attachment_metadata', 'dos_thumbnail_upload', 100, 1);
-add_filter('wp_get_attachment_url', 'dos_attachment_url');
-
-if ( get_option('dos_storage_file_delete') == 1 ) {
-  add_filter('wp_delete_file', 'dos_storage_delete', 10, 1);
-}
